9d5b4dd9f62f37597458ba4e1976dd33577fe2d8
jt-tirado <jt.tirado@gmail.com>
2018-05-06 18:23:52 -0700

    Create userSetup.py

---
+++ userSetup.py
@@ -0,0 +1,5 @@
+import maya.cmds as cmds
+cmds.evalDeferred("import lancer.menu")
+cmds.evalDeferred("lancer.menu.show()")
+cmds.evalDeferred("import lancer.channelBox.ui")
+cmds.evalDeferred("lancer.channelBox.ui.show()")
