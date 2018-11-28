dc61645ffe24043540608098ebe4e35fb5ce50a7
jt-tirado <jt.tirado@gmail.com>
2018-11-27 17:58:20 -0800

    Update keys.py

--- channelBox/keys.py
+++ channelBox/keys.py
@@ -58,7 +58,84 @@ def detectThreshold(value, prev, next, threshold):
             return True
         elif prev < value >= next - threshold:
             return True
-    return False
+        else:
+            return False
+    else:
+        return False
+
+
+def detectTrajectory(value, prev, next):
+    if prev < value < next:
+        midPoint = (next - prev) / 2 + prev
+        if value == midPoint:
+            return True
+        else:
+            return False
+    elif prev > value > next:
+        midPoint = (prev - next) / 2 - prev
+        if value == midPoint:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def deleteKeysDecorator(function, *args, **kwargs):
+    '''
+    Delete Keys on selected objects based on function.
+    Function must return bool (current, prev, next).
+    :param function:
+    :param args:
+    :param kwargs:
+    :return:
+    '''
+
+    def wrapper(*args, **kwargs):
+        selected = cmds.ls(sl=True)
+
+        if selected:
+            for item in selected:
+                for attr in cmds.listAttr(item, k=True):
+                    attribute = '{}.{}'.format(item, attr)
+
+                    if attributeHasAnimation(attribute):
+                        frames = cmds.keyframe(item, q=1, at=attr)
+                        values = cmds.keyframe(item, q=1, at=attr, vc=1)
+                        classArray = []
+                        crushKeys = []
+
+                        i = 0
+                        while i < len(frames):
+                            classArray.append(Keyframe(frames[i], values[i]))
+                            i += 1
+
+                        i = 1
+                        while i < (len(classArray) - 1):
+                            keyA = classArray[i - 1].getValue()
+                            keyB = classArray[i].getValue()
+                            keyC = classArray[i + 1].getValue()
+
+                            if function(keyB, keyA, keyC, *args, **kwargs):
+                                crushKeys.append(classArray[i].getFrame())
+                            i += 1
+
+                        for frame in crushKeys:
+                            cmds.cutKey(item, at=attr, cl=1, t=(frame, frame))
+    return wrapper
+
+
+########################################################################################################################
+#
+#
+#	Delete Static Channels
+#
+#
+########################################################################################################################
+
+def deleteStaticAllChannels(*args, **kwargs):
+    cmds.delete(all=True, staticChannels=True)
+    return
 
 
 ########################################################################################################################
@@ -69,68 +146,13 @@ def detectThreshold(value, prev, next, threshold):
 #
 ########################################################################################################################
 
+# TODO: Add Trajectory
+# TODO: Add Threshold
+# TODO: Detect anim layers feature
 
-def deleteRedundant(item, attribute, threshold=None, *args):
-    # TODO: Add Threshold
-    # TODO: Detect anim layers feature
-
-    if attributeHasAnimation('{}.{}'.format(item, attribute)):
-        frames = cmds.keyframe(item, q=1, at=attribute)
-        values = cmds.keyframe(item, q=1, at=attribute, vc=1)
-        classArray = []
-        crushKeys = []
-
-        i = 0
-        while i < len(frames):
-            newKey = Keyframe(frames[i], values[i])
-            classArray.append(newKey)
-            i += 1
-
-        i = 1
-
-        while i < (len(classArray) - 1):
-            keyA = classArray[i - 1].getValue()
-            keyB = classArray[i].getValue()
-            keyC = classArray[i + 1].getValue()
-            if keyB == keyA and keyB == keyC:
-                crushKeys.append(classArray[i].getFrame())
-            # else:
-            #     if threshold:
-            #         if detectThreshold(value=float(keyB),
-            #                            prev=float(keyA),
-            #                            next=float(keyC),
-            #                            threshold=float(threshold)
-            #                            ):
-            #             crushKeys.append(classArray[i].getFrame())
-            i += 1
-
-        for frame in crushKeys:
-            cmds.cutKey(item, at=attribute, cl=1, t=(frame, frame))
-    return
-
-
-def deleteAllRedundant(*args):
-    selected = cmds.ls(sl=True)
-
-    if selected:
-        amount = 0
-        maxAmount = len(selected)
-
-        cmds.progressWindow(title='Delete Redundant Keys',
-                            progress=amount,
-                            max=maxAmount,
-                            status='Deleting Keys',
-                            isInterruptable=True)
-
-        for item in selected:
-            amount += 1
-            cmds.progressWindow(edit=True, progress=amount, status='Deleting Keys on: {}'.format(item))
-
-            if cmds.progressWindow(query=True, isCancelled=True):
-                break
-            else:
-                for attr in cmds.listAttr(item, k=True):
-                    deleteRedundant(item, attr, 0.001)
-
-        cmds.progressWindow(endProgress=True)
-    return
+@deleteKeysDecorator
+def deleteRedundant(current, prev, next, *args, **kwargs):
+    if current == prev and current == next:
+        return True
+    else:
+        return False
