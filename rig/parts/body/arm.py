8aff7ccd5bd041bdc4f376ad03c17b31b5dc2863
jt-tirado <jt.tirado@gmail.com>
2019-03-05 16:46:34 -0800

    Update arm.py

--- rig/parts/body/arm.py
+++ rig/parts/body/arm.py
@@ -2,166 +2,198 @@
 from rig.utils import *
 from rigBase import RIGBASE
 from digit import DIGIT
+from rig.parts.baseRig import BASERIG
 
-# Maya Moudles
+# Maya Modules
 from maya import cmds
 
 
-class ARM(RIGBASE):
+class ARM(BASERIG):
 	def __init__(self,
-	             side,
-	             shoulder,
-	             elbow,
-	             hand,
-	             collar=None,
-	             networkRoot=None,
-	             name=Part.arm,
-	             index=0,
-	             ):
-		RIGBASE.__init__(self,
-		                 networkRoot=networkRoot,
-		                 name=name,
-		                 side=side,
-		                 index=index,
-		                 )
+				 collar,
+				 shoulder,
+				 elbow,
+				 hand,
+				 side=Position.left,
+				 parent=None,
+				 root=None,
+				 ):
+		items = [collar, shoulder, elbow, hand]
 
-		self.collar = collar
-		self.collarFKControl = None
-		self.collarFKGroup = None
-		self.collarIKControl = None
-		self.collarIKGroup = None
+		# Items
+		self.collarItem = collar
+		self.shoulderItem = shoulder
+		self.elbowItem = elbow
+		self.handItem = hand
 
-		self.shoulder = shoulder
-		self.elbow = elbow
-		self.hand = hand
-		self.objects = [shoulder, elbow, hand]
+		self.armItems = [shoulder, elbow, hand]
 
-		self.getScale()
-		self.createFKIKChain(self.objects)
-
-
-		#self.createTwistChain(self.shoulder, self.elbow, self.hand)
-
-		if self.collar:
-			self.createDetailChain(self.collar)
-			self.createCollar()
-
-		self.createGrandchildren([self.shoulder, self.elbow, self.hand])
-
-		# FIXME: HAND Class
-		#self.createHand()
-		self.createLocalWorld(obj=self.fkControl[0],
-		                      local=self.fkGroup[0],
-		                      )
-		self.createSet([self.collarFKControl] + self.fkControl + self.ikControl)
-		self.createNetwork(typ=self.name)
-
-		self.createNetworkConnections()
-		self.updateNetwork()
-
-	def getScale(self):
-		self.scale = rigging.getDistance(self.shoulder, self.elbow) / 2.5
-		return
-
-	def createCollar(self):
-		ctl = CONTROL(name=longName(self.name,
-		                            self.side[0],
-		                            self.index,
-		                            Component.fk,
-		                            Part.collar.capitalize(),
-		                            Component.control,
-		                            ),
-		              typ=WireType.dumbbell,
-		              scale=self.scale,
-		              axis=[1, 0, 0],
-		              child=self.collar,
-		              side=self.side,
-		              label=Part.collar,
-		              color=Component.fk,
-		              )
-		rigging.lockScale(ctl.transform)
-		cmds.parentConstraint(ctl.transform, self.collar)
-
-		parent = cmds.listRelatives(self.collar, parent=True)
-		if parent:
-			cmds.parent(ctl.group, parent[0])
-
-		self.collarFKControl = ctl.transform
-		self.collarFKGroup = ctl.group
-
-		self.objects.insert(0, self.collar)
-		return
-
-	def createHand(self):
-		HAND(hand=self.hand,
-		     side=self.side,
-		     networkRoot=self.networkRoot,
-		     index=self.index
-		     )
-		return
-
-	def updateNetwork(self):
 		# Collar
-		if self.collar:
-			self.connectToNetwork(self.collar, self.network, 'skeletonCollar')
+		self.collarFKControl = None
+		self.collarIKControl = None
+		self.collarJoint = None
 
-		if self.collarFKControl:
-			self.connectToNetwork(self.collarFKControl, self.network, 'collarFkControl')
+		BASERIG.__init__(self,
+						 prefix=Part.arm,
+						 side=side,
+						 kind=Part.arm,
+						 items=items,
+						 parent=parent,
+						 root=root,
+						 axis=[1, 1, 0],
+						 )
+
+	####################################################################################################################
+	# Properties
+	####################################################################################################################
+
+	@property
+	def allControls(self):
+		result = BASERIG.allControls
+
+		for ctrl in flatList(self.collarFKControl,
+							 self.collarIKControl,
+							 ):
+
+			if isinstance(ctrl, object):
+				for attr in ['transform', 'offsetTransform']:
+					if hasattr(ctrl, attr):
+						value = getattr(ctrl, attr)
+						if value:
+							result.append(value)
+			elif ctrl is not None:
+				result.append(ctrl)
+		return
+
+	####################################################################################################################
+	# Methods
+	####################################################################################################################
+
+	def create(self, *args, **kwargs):
+		# Scale
+		self.scale = self.scaleByDistance(self.items) * 1.5
+
+		# Interface
+		self.interface = self.createInterfaceControl(child=self.items[-1])
+
+		# Collar
+		self.createCollarControl()
+
+		# Arm
+		self.create3PointFKIK(items=self.armItems)
+
+		# Child Controls
+		self.createChildChain(self.items)
+
+		# Hand
+		self.createHandControl()
+
+		# Local World
+		# TODO: Arm Local World
+		if self.root and self.parent:
+			pass
+
+		# self.createLocalWorld(obj=self.fkControl[0],
+		# 					  local=self.fkGroup[0],
+		# 					  )
+		return
+
+	def createCollarControl(self):
+		# Joint
+		self.collarJoint = Joint(prefix=self.prefix,
+								 name=Part.collar,
+								 side=self.side,
+								 kind=camelCase(Component.rig, Component.joint, capitalize=False),
+								 drawStyle=JointDrawStyle.none,
+								 item=self.collarItem
+								 )
+
+		self.collarJoint.snapTo(self.collarItem)
+		self.collarJoint.freezeTransforms()
+
+		# FK Control
+		self.collarFKControl = FKCONTROL(prefix=self.prefix,
+										 side=self.side,
+										 name=Part.collar,
+										 wire=WireType.dumbbell,
+										 scale=self.scale,
+										 item=self.collarJoint,
+										 )
+
+		# TODO: Arm Collar IK Control
+		self.collarIKControl = None
+
+		# Constrain
+		cmds.parentConstraint(self.collarJoint, self.collarItem, mo=True)
+		cmds.scaleConstraint(self.collarJoint, self.collarItem, mo=True)
+
+		self.collarFKControl.constrainItem(point=True,
+										   orient=True,
+										   scale=True,
+										   offset=True
+										   )
+		return
+
+	def createHandControl(self):
+		HAND(hand=self.handItem, interface=self.interface)
 		return
 
 
-class HAND(RIGBASE):
+class LEFTARM(ARM):
+	def __init__(self, side=Position.left, *args, **kwargs):
+		ARM.__init__(self, side=side, *args, **kwargs)
+
+
+class RIGHTARM(ARM):
+	def __init__(self, side=Position.right, *args, **kwargs):
+		ARM.__init__(self, side=side, *args, **kwargs)
+
+
+class HAND(AbstractNode):
 	def __init__(self,
-	             hand,
-	             side,
-	             networkRoot=None,
-	             name=Part.hand,
-	             index=0,
-	             ):
-		RIGBASE.__init__(self,
-		                 items=rigging.listCheck(hand),
-		                 networkRoot=networkRoot,
-		                 name=name,
-		                 side=side,
-		                 index=index,
-		                 )
+				 hand,
+				 interface=None,
+				 *args,
+				 **kwargs
+				 ):
+
+		AbstractNode.__init__(self, *args, **kwargs)
 
 		self.hand = hand
+		self.interface = interface
+		self.thumb = None
+		self.indexFinger = None
+		self.middleFinger = None
+		self.ringFinger = None
+		self.pinkyFinger = None
+
 		self.finger = []
-		self.createFingers()
 
-		self.createNetwork(typ=longName(self.name,
-		                                self.side.upper()[0],
-		                                self.index,
-		                                )
-		                   )
-		self.updateNetwork()
+		self.create()
+		self.finalize()
 
-	def createFingers(self):
+	def create(self):
 		children = getJointChildren(self.hand)
 
 		i = 0
 		for child in children:
 			joints = getJointOrder(child)
 			finger = DIGIT(items=joints,
-			               name=Part.finger,
-			               side=self.side,
-			               networkRoot=self.networkRoot,
-			               index=i,
-			               )
+						   name=Part.finger,
+						   side=self.side,
+						   networkRoot=self.networkRoot,
+						   index=i,
+						   )
 			self.finger.append(finger)
 			i += 1
 		return
 
-	def updateNetwork(self):
+	def finalize(self):
+		if self.interface:
+			pass
 		return
 
 
-class LEFTARM(ARM):
-	def __init__(self, *args, **kwargs):
-		ARM.__init__(self, side=Position.left, *args, **kwargs)
 
 
-class RIGHTARM(ARM):
-	def __init__(self, *args, **kwargs):
-		ARM.__init__(self, side=Position.right, *args, **kwargs)
+
