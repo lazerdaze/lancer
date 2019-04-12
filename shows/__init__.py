89515bfcae5c1a52a8c7d27855c7f7edd3d929f1
jt-tirado <jt.tirado@gmail.com>
2019-04-11 18:23:36 -0700

    Update __init__.py

--- shows/__init__.py
+++ shows/__init__.py
@@ -1,1 +1,16 @@
+import kia
+import energizer
+import apex
+reload(kia)
+reload(energizer)
+reload(apex)
 
+from maya import cmds
+
+
+def sub_menu(*args, **kwargs):
+	cmds.menuItem(l='Shows', subMenu=True, to=True)
+	cmds.menuItem(label='Apex Legends', d=True)
+	cmds.menuItem(label='Ingest Tool', command=apex.Ingest_UI)
+	cmds.setParent('..', menu=True)
+	return
